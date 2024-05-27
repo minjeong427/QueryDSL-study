ca1e89211d3c7e156739b592761161f54f7f7e07
minjeong427 <bling427@naver.com>
2024-05-27 16:14:36 +0900

    #3. QueryDSL 객체화(interface, ImplClass), 다양한 문법 학습(그룹함수, 페이징, 조인, 서브쿼리)

---
+++ src/main/java/com/example/study/repository/MemberRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.example.study.repository;
+
+import com.example.study.entity.Member;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+
+    // JPA꺼 아님!! 상속 안받음.
+    List<Member> findByName(String name);
+
+}
